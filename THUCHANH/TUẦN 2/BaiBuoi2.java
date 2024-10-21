e1fc62c943a62f5556e49d6aba342e067a5b19dd
Buinamphuong <namphuongbui0508@gmail.com>
2024-10-21 21:38:46 +0700

    Add files via upload

---
+++ THUCHANH/TUẦN 2/BaiBuoi2.java
@@ -0,0 +1,18 @@
+public class BaiBuoi2{
+    public static void main(String[] args) {
+        String maSinhVien = "109830", hoVaTen = "Bùi Nam Phương", ngaySinh ="05/08/2006", lop = "GD24A", khoa ="Mỹ thuật ứng dụng", diaChi ="Le Anh Xuan" ;
+        int tuoi = 18;
+        boolean gioitinh = false;
+        System.out.println("------THÔNG TIN SINH VIÊN-----");
+        System.out.println("Mã sinh viên: " + maSinhVien);
+        System.out.println("Họ và tên: " + hoVaTen);
+        System.out.println("Ngày sinh: " + ngaySinh);
+        System.out.println("Lớp: " + lop);
+        System.out.println("Khoa: " + khoa);
+        System.out.println("Địa chỉ: " + diaChi);
+System.out.println("Giới tính: " + gioitinh);
+System.out.println("Tuổi: " + tuoi);
+System.out.println("-----Hết-----");
+
+}
+}
